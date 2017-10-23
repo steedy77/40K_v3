914258e24685694b37a579831013851960ebd7f7
steedy77 <hayden_steedman@hotmail.com>
2017-10-23 16:26:51 +1300

    Chaplin Work
    Axe swipe affect
    Animated swipe in combo anim

---
+++ Assets/Scripts/ParticleEmit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEmit : MonoBehaviour {
+
+    public ParticleSystem AxeSwipe;
+	
+	
+	// Update is called once per frame
+	void Update () {
+    AxeSwipe.Emit(300);
+}
+}
